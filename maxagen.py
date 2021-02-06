7077517d8de2e31968825c455188441133866d18
Tengz <redcraftrus@gmail.com>
2021-02-06 21:54:14 +0200

    Qt added

--- maxagen.py
+++ maxagen.py
@@ -1,5 +1,74 @@
 """Maxa text generator."""
 import random
+import sys
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QPushButton, QCheckBox, QLineEdit
+
+
+class ResultWindow(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        self.title = "Result"
+        self.top = 100
+        self.left = 100
+        self.width = 1000
+        self.height = 500
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.top, self.left, self.width, self.height)
+
+        self.text_out = QLabel(self)
+        self.text_out.move(10, 10)
+        self.text_out.setStyleSheet("background-color: white")
+
+
+class FirstWindow(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        self.show_result = None
+
+        self.title = "MaxxaGenetator 0.9"
+        self.top = 100
+        self.left = 100
+        self.width = 400
+        self.height = 200
+
+        self.button = QPushButton("Start", self)
+        self.button.move(10, 100)
+        self.button.clicked.connect(self.go)
+
+        self.all_phrases_label = QLabel("All Phrases count:", self)
+        self.all_phrases_label.move(10, 10)
+        self.all_phrases = QLineEdit(self)
+        self.all_phrases.move(100, 10)
+        self.all_phrases.resize(50, 25)
+
+        self.line_phrases_label = QLabel("Line Phrases count:", self)
+        self.line_phrases_label.move(10, 40)
+        self.line_phrases = QLineEdit(self)
+        self.line_phrases.move(110, 40)
+        self.line_phrases.resize(50, 25)
+
+        self.cap = QCheckBox("Capitalize first letters?", self)
+        self.cap.move(10, 70)
+        self.cap.resize(200, 25)
+
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.top, self.left, self.width, self.height)
+        self.show()
+
+    def go(self):
+        text = union_random(int(self.all_phrases.text()), int(self.line_phrases.text()), self.cap.isChecked())
+
+        self.show_result = ResultWindow()
+        self.show_result.text_out.setText(text)
+        self.show_result.text_out.adjustSize()
+        self.show_result.text_out.setTextInteractionFlags(Qt.TextSelectableByMouse)
+        self.show_result.show()
 
 
 def read_from_file() -> list:
@@ -36,5 +105,7 @@ def union_random(all_count, line_count, capitalize):
     return res_str
 
 
-# union_random(all phrases count, phrases in line count, capitalize first char on line)
-print(union_random(20, 2, 1))
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    mainWindow = FirstWindow()
+    sys.exit(app.exec())
