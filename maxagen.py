5139e41ad9f175fd3ff24836b2223c833f975f03
tengz <redcraftrus@gmail.com>
2021-04-14 23:56:42 +0300

    a lot of changes, !!! BETA version !!!

--- maxagen.py
+++ maxagen.py
@@ -40,7 +40,7 @@ class MaxxaGen:
         while counter != self.all_phrs:
             counter += 1
 
-            phrase = self.get_random(False) if self.allow_repeats else self.get_random(True)
+            phrase = self.get_random(False) if self.allow_repeats is True else self.get_random(True)
 
             if phrase:
                 if line_counter == self.line_phrs:
@@ -66,7 +66,7 @@ class MaxxaGen:
         for val in res_dict.values():
             if res_str:
                 res_str += "\n"
-            res_str += val.capitalize() if self.captlz else val
+            res_str += val.capitalize() if self.captlz is True else val
 
         return res_str
 
@@ -100,40 +100,72 @@ async def on_ready():
 async def on_message(message):
     # list of bot commands
     commands = ['$help', '/ген', '$gen', '/йоу', '$hi', '/прощение', '$sry']
+    sorry_commands = ["прощение", "sry", "sorry"]
+    gen_commands = ["gen", "ген"]
+    help_commands = ["help", "помощь", "помогите"]
+    hello_commands = ["привет", "здарова", "hello", "йоу"]
+
+    msg_content_splitted = message.content.lower().split(",")
+    msg_content = msg_content_splitted[0]
+    #
+    all_count = int(msg_content_splitted[1]) if len(msg_content_splitted) >= 2 else 5
+    line_count = int(msg_content_splitted[2]) if len(msg_content_splitted) >= 3 else 5
+    cap = msg_content_splitted[3] if len(msg_content_splitted) >= 4 else True   # TODO: Converting to bool
+    rep = msg_content_splitted[4].bool if len(msg_content_splitted) == 5 else True
+
+    # print(all_count, line_count, cap, rep)
+
+    st = {
+        "all_phrases": all_count,
+        "line_phrases": line_count,
+        "capitalize": cap,
+        "allow_repeats": rep,
+        "file": "phrases.txt",
+    }
+
     # ignore bot messages
     if message.author == client.user:
         return
 
     # help.txt reading to string
-    with open("help.txt", "r") as file:
+    with open("help.txt", "r", encoding="utf8") as file:
         data = file.read()
+
 
     # mahammad20 messages duplicator
     if message.author.id == 373115287828037632:
         await message.channel.send(f"```{message.content}```")
+    # if message.author.id == 373115287828037632:
+    #     await message.channel.send(f"```{message.content}```")
+    if msg_content.startswith('/'):
+        # delete command message
+        if msg_content.startswith('/?'):
+            await message.delete()
 
-    # checks if dolboeb maha writes command without mistakes
-    if message.author.id == 373115287828037632 and message.content.startswith(tuple(commands)) and " " in message.content:
-        await message.channel.send("Махмуд пошёл нахуй, читай команды долбаёб")
-    # if maha asks somebody say "jo", bot will print "jo" 60 times in 1 msg
-    elif message.author.id == 373115287828037632 and 'пойоукай' in message.content:
-      await message.channel.send('йоу ' * 60)
-    # $gen command
-    elif message.content == '/ген' or message.content == '$gen':
-        await message.channel.send(MaxxaGen(settings_gen))
-    # $sry command
-    elif message.content.startswith('/прощение') or message.content.startswith('$sry'):
-        await message.channel.send(MaxxaGen(settings_forgiveness))
-    # $help command
-    elif message.content.startswith('$help'):
-        await message.channel.send(f"```{data}```")
-    # $hi command
-    elif message.content.startswith('/йоу') or message.content.startswith('$hi'):
-        await message.channel.send("йоу бандишка братишка") # добавить файл с разными приветствиями и принтить рандомное
-    # skips written command /play (Rhytm bot) by maha
-    elif message.author.id == 373115287828037632 and '/play' in message.content:
-        pass
+        # checks if dolboeb maha writes command without mistakes
+        if message.author.id == 373115287828037632 and message.content.startswith(tuple(commands)) and " " in message.content:
+            await message.channel.send("Махмуд пошёл нахуй, читай команды долбаёб")
+        # if maha asks somebody say "jo", bot will print "jo" 60 times in 1 msg
+        elif message.author.id == 373115287828037632 and 'пойоукай' in message.content:
+          await message.channel.send('йоу ' * 60)
+        # $gen command
+        elif any(ele in msg_content for ele in gen_commands):
+            print(st)
+            print(MaxxaGen(st))
+            await message.channel.send(MaxxaGen(st))
+        # $sry command
+        elif any(ele in msg_content for ele in sorry_commands):
+            await message.channel.send(MaxxaGen(settings_forgiveness))
+        # $help command
+        elif any(ele in msg_content for ele in help_commands):
+            await message.channel.send(f"```{data}```")
+        # $hi command
+        elif any(ele in msg_content for ele in hello_commands):
+            await message.channel.send("йоу бандишка братишка") # добавить файл с разными приветствиями и принтить рандомное
+        # skips written command /play (Rhytm bot) by maha
+        elif message.author.id == 373115287828037632 and '/play' in message.content:
+            pass
 
 
 
-client.run('ODMxMDcyOTkwMzc1OTY4NzY5.YHP7CQ.l2KQwWSyEicZTshN14JQKmHrzLA')
+client.run('ODMxNjQ3OTAyNjkzOTgyMjI4.YHYSdw.i7Eduu37I0bHcHr8bUWjmRRe4lY')
